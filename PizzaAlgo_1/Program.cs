aa7ed2e23156efa75d1ca38b6b6e8085a58e5e89
IVProb <jnoojinjr@gmail.com>
2021-03-25 22:49:29 -0500

    added the steps in the algorithm

--- PizzaAlgo_1/Program.cs
+++ PizzaAlgo_1/Program.cs
@@ -6,7 +6,15 @@ namespace PizzaAlgo_1
     {
         static void Main(string[] args)
         {
-            
+            // Algo Excercis
+            // 1) Place call to Mario's Pizza
+            // 2) Is this pick up or delviery
+            // 3) Is this thing crust, deep dish, or regular crust
+            // 4) What toppings would you like?
+            // 5) Anything extra, like breadsticks or dipping sauce?
+            // 6) How to pay, cash or card?
+            // 7) If card && delivery, would you like to tip the driver?
+
         }
     }
 }
