28d0c6a1b9e8b6e3b2a32fadbf42c16737a9b4dd
IVProb <jnoojinjr@gmail.com>
2021-03-25 22:53:46 -0500

    Edited steps

--- PizzaAlgo_1/Program.cs
+++ PizzaAlgo_1/Program.cs
@@ -7,11 +7,11 @@ namespace PizzaAlgo_1
         static void Main(string[] args)
         {
             // Algo Excercis
-            // 1) Place call to Mario's Pizza
-            // 2) Is this pick up or delviery
-            // 3) Is this thing crust, deep dish, or regular crust
-            // 4) What toppings would you like?
-            // 5) Anything extra, like breadsticks or dipping sauce?
+            // 1) Decide what pizzaria to call
+            // 2) Decide if this is pick up or delviery
+            // 3) Decide what toppings to add
+            // 4) Decide what type of crust
+            // 5) Make choice to choose anything extra, like breadsticks or dipping sauce?
             // 6) How to pay, cash or card?
             // 7) If card && delivery, would you like to tip the driver?
 
